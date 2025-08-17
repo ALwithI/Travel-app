43e8aee9b0b067805c2e6a7329de0f76130bf72f
ALwithI <155818448+ALwithI@users.noreply.github.com>
2025-08-17 20:12:19 +0100

    auth finished

--- auth.ts
+++ auth.ts
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-export const {}= NextAuth(
+
+import prisma from "@/lib/prisma"
+export const {auth,handlers,signIn,signOut}= NextAuth(
     {
         providers: [Github],
-        adapter:PrismaAdapter()
+        adapter:PrismaAdapter(prisma)
     }
 )
