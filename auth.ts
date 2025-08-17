0f40cacf179aef4f053ea5181880c243619e6972
ALwithI <155818448+ALwithI@users.noreply.github.com>
2025-08-17 20:04:31 +0100

    adding nav bar and prisma ORM

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth"
+import Github from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+export const {}= NextAuth(
+    {
+        providers: [Github],
+        adapter:PrismaAdapter()
+    }
+)
