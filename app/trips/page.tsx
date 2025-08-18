b764fb16c5889bb0de453c0d63e36c03a0f625f7
ALwithI <155818448+ALwithI@users.noreply.github.com>
2025-08-18 23:11:32 +0100

    create travel card finished

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,9 +1,10 @@
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function TripsPage() {
   const session = await auth();
-  if (session) {
+  if (!session) {
     return (
       <div className="flex justify-center items-center h-screen text-gray-700 text-xl">
         Please Sign In
@@ -14,7 +15,9 @@ export default async function TripsPage() {
     <div className="space-y-6 container mx-auto px-4 py-8">
       <div>
         <h1>Dashboard</h1>
-        <Button>New Trip</Button>
+        <Link href="trips/new">
+          <Button>New Trip</Button>
+        </Link>
       </div>
     </div>
   );
