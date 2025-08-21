2307f276dfc5eebccd2927e3232cf292df966ba3
ALwithI <155818448+ALwithI@users.noreply.github.com>
2025-08-21 23:19:03 +0100

    adding new location component

--- app/trips/[tripId]/itinerary/new/page.tsx
+++ app/trips/[tripId]/itinerary/new/page.tsx
@@ -1,8 +1,10 @@
-export default function NewLocation({
+import NewLocationnClient from "@/components/New-location";
+export default async function NewLocation({
   params,
 }: {
   params: Promise<{ tripId: string }>;
 }) {
   const { tripId } = await params;
-  return;
+
+  return <NewLocationnClient tripId={tripId} />;
 }
---
+++ components/New-location.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function NewLocationClient({ tripId }: { tripId: string }) {
+  return (
+    <div>
+      <h1>New Location</h1>
+    </div>
+  );
+}
